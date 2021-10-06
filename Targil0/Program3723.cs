4990160218ba05d779fa9050334aa9f4aaee4829
pinskishira <pinski@g.jct.c.il>
2021-10-06 22:54:56 +0300

    initial code + welcome functions

---
+++ Targil0/Program3723.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome3723()
+        {
+            Console.WriteLine("Hi I am Shira Pinski, i am also here!");
+        }
+    }
+}
