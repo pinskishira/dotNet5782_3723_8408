5b4025d244e3b1fc318c6328c8937b0e84706135
yiratb <yibiton@g.jct.ac.il>
2021-10-06 22:28:51 +0300

    Initial code + Welcome Functions

---
+++ Targil0/Program8408.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8408();
+            Welcome3723();
+            Console.ReadKey();
+        }
+        static partial void Welcome3723();
+        private static void Welcome8408()
+        {
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", username);
+        }
+    }
+}
