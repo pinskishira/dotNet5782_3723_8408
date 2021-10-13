2177e0dba6c19a3635f887ccb66bf67c48f1ebb0
pinskishira <pinski@g.jct.c.il>
2021-10-13 18:07:52 +0300

    i made a change

---
+++ DAL/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DAL.DO;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+            int id;
+            string model;
+            WeightCategories MaxWeight;
+            DroneStatuses Status;
+            double Battery;
+        }
+
+        public struct Station
+        {
+            int Id;
+            int Name;
+            double Longitude;
+            double Lattitude;
+            int ChargeSlots;
+        }
+
+        public struct DroneCharge
+        {
+            int Droneld;
+            int Stationld;
+        }
+    }
+    public class Class1
+    {
+    }
+}
+
+namespace DalObject
+{
+    public struct DataSource
+    {
+        static internal Drone[] Drone = new Drone [10];
+    }
+}
