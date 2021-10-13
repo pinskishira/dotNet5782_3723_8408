47d144e5286ace213f3348e5e6aa09797fce2f4e
yiratb <yibiton@g.jct.ac.il>
2021-10-13 17:54:54 +0300

    I added structs

---
+++ DAL/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DAL.DO;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+
+        }
+        public struct Customer
+        {
+            int Id;
+            string Name;
+            String Phone;
+            double Longitude;
+            double Lattitude;
+        }
+        public struct parcel
+        {
+            int Id;
+            int Senderld;
+            int Targetld;
+            WeightCategories Weight;
+            Priorities Priority;
+            datetime Requested;
+            int Droneld;
+            datetime Scheduled;
+            datetime PickedUp;
+            datetime Delivered;
+        }
+    }
+    public class Class1
+    {
+    }
+}
+namespace DalObject
+{
+    public class DataSource
+    {
+        static internal Drone[] drone=new Drone[10];
+        //int[] arr=new int[8];
+    }
+}
