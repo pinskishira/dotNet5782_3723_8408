f42d606cc1ebf0043513514c79a67afb8c57b337
pinskishira <pinski@g.jct.c.il>
2021-11-03 17:37:48 +0200

    changing

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -10,11 +10,11 @@ namespace DalObject
     public static class DataSource
     {
         static Random rand = new Random();
-        static List<Drone> Drones = new();//Defining an array of size 10 for the drones
-        static List<Station> Stations = new();//Defining an array of size 5 for the stations
-        static List<Customer> Customers = new();//Defining an array of size 100 for the customers
-        static List<Parcel> Parcels = new();//Defining an array of size 10000 for the parcels
-        static List<DroneCharge> DroneCharges = new();//Defining an array of size 100 for the drone charges
+        static List<Drone> Drones = new();//Defining a list for the drones
+        static List<Station> Stations = new();//Defining a list for the stations
+        static List<Customer> Customers = new();//Defining a list for the customers
+        static List<Parcel> Parcels = new();//Defining a list for the parcels
+        static List<DroneCharge> DroneCharges = new();//Defining a list for the drone charges
         /// <summary>
         /// Has all my static indexes
         /// </summary>
@@ -33,49 +33,57 @@ namespace DalObject
         /// </summary>
         public static void Initialize()
         {
+            Station newStation = new();
+            string[] stationArrayNames = new string[2];
+            stationArrayNames[0] = "Bayit Vegan";
+            stationArrayNames[1] = "Givat Shaul";
             for (int loopStation = 0; loopStation < 2; loopStation++)//Updating 2 base stations
             {
-                Stations[loopStation].Id = rand.Next(1000, 10000);//Updating 4-digit ID name 
+                newStation.Id = rand.Next(1000, 10000);//Updating 4-digit ID name                                   
                 for (int help = 0; help < loopStation; help++)//Checking if it already appears in array
                 {
-                    while (Stations[loopStation].Id == Stations[help].Id)
-                        Stations[loopStation].Id = rand.Next(1000, 10000);
+                    while (newStation.Id == Stations[help].Id)
+                        newStation.Id = rand.Next(1000, 10000);
                 }
-                Stations[loopStation].Longitude = 35 + rand.NextDouble();//Updating longitude                                     
-                Stations[loopStation].Latitude = 31 + rand.NextDouble();//Updating latitude    
-                Stations[loopStation].ChargeSlots = rand.Next(10, 30);//Updating charging slots
+                newStation.Longitude = 35 + rand.NextDouble();//Updating longitude                                     
+                newStation.Latitude = 31 + rand.NextDouble();//Updating latitude    
+                newStation.ChargeSlots = rand.Next(10, 30);//Updating charging slots
+                newStation.Name = stationArrayNames[loopStation];
+                Stations.Add(newStation);
             }
-            Stations[0].Name = "Bayit Vegan";//Updating names
-            Stations[1].Name = "Givat Shaul";
             Config.IndexStation = 2;
 
-            for (int i = 0; i < 5; i++)//Updating 5 drones
+            Drone newDrone = new();
+            string[] droneArayNames = new string[5];
+            droneArayNames[0] = "123EST";
+            droneArayNames[1] = "234EST";
+            droneArayNames[2] = "345EST";
+            droneArayNames[3] = "456EST";
+            droneArayNames[4] = "567EST";
+            for (int loopDrone = 0; loopDrone < 5; loopDrone++)//Updating 5 drones
             {
-                Drones[i].Id = rand.Next(10000, 100000);//Updating 5-digit ID name 
-                for (int j = 0; j < i; j++)//Checking if it already appears in array
+                newDrone.Id = rand.Next(10000, 100000);//Updating 5-digit ID name 
+                for (int j = 0; j < loopDrone; j++)//Checking if it already appears in array
                 {
-                    while (Drones[i].Id == Drones[j].Id)
-                        Drones[i].Id = rand.Next(10000, 100000);
+                    while (newDrone.Id == Drones[j].Id)
+                        newDrone.Id = rand.Next(10000, 100000);
                 }
-                Drones[i].MaxWeight = (WeightCategories)rand.Next(1, 4);//Updating the weight category
-                Drones[i].Battery = rand.Next(1, 101);//Updating battery status
+                newDrone.MaxWeight = (WeightCategories)rand.Next(1, 4);//Updating the weight category
+                newDrone.Model = droneArayNames[loopDrone];//Updating model
+                //newDrone.Battery = rand.Next(1, 101);//Updating battery status
                 // Updating drone status
-                if (Drones[i].Battery < 20)
-                {
-                    Drones[i].Status = DroneStatuses.Maintenance;
-                    int station = rand.Next(2);
-                    DroneCharges[Config.IndexDroneCharge].StationId = Stations[station].Id;
-                    Stations[station].ChargeSlots--;
-                }
-                else
-                    Drones[i].Status = DroneStatuses.Available;
+                //if (newDrone.Battery < 20)
+                //{
+                //    newDrone.Status = DroneStatuses.Maintenance;
+                //    int station = rand.Next(2);
+                //    DroneCharges[Config.IndexDroneCharge].StationId = Stations[station].Id;
+                //    Stations[station].ChargeSlots--;
+                //}
+                //else
+                //    newDrone.Status = DroneStatuses.Available;
             }
-            Drones[0].Model = "123EST";//Updating model
-            Drones[1].Model = "234EST";
-            Drones[2].Model = "345EST";
-            Drones[3].Model = "456EST";
-            Drones[4].Model = "567EST";
             DataSource.Config.IndexDrone = 5;//Promoting the index
+
             for (int i = 0; i < 10; i++)//Updating 10 customers
             {
                 Customers[i].Id = rand.Next(100000000, 1000000000);//Updating ID name randomly
@@ -112,43 +120,44 @@ namespace DalObject
             Customers[9].Phone = "0506929115";
 
             ref int parcelNum = ref Config.NextParcelNumber;
+            Parcel newParcel = new();
             for (int index = 0; index < 10; index++)//Updating 10 parcels
             {
-                Parcels[index].Id = Config.NextParcelNumber++;//Updating the ID number of the package
-                Parcels[index].SenderId = Customers[rand.Next(10)].Id;//Updating the ID number of the sender
-                Parcels[index].DroneId = 0;//Updating the ID number of the drone
+                newParcel.Id = Config.NextParcelNumber++;//Updating the ID number of the package
+                newParcel.SenderId = Customers[rand.Next(10)].Id;//Updating the ID number of the sender
+                newParcel.DroneId = 0;//Updating the ID number of the drone
                 do
                 {
-                    Parcels[index].TargetId = Customers[rand.Next(10)].Id;
+                    newParcel.TargetId = Customers[rand.Next(10)].Id;
                 }
                 while (Parcels[index].SenderId == Parcels[index].TargetId);
 
-                Parcels[index].Weight = (WeightCategories)rand.Next(1,4);//Updating the weight
-                Parcels[index].Priority = (Priorities)rand.Next(1,4);//Updating the urgency of the shipment
+                newParcel.Weight = (WeightCategories)rand.Next(1,4);//Updating the weight
+                newParcel.Priority = (Priorities)rand.Next(1,4);//Updating the urgency of the shipment
                 //Putting a random date and time
-                Parcels[index].Requested = new DateTime(2021, rand.Next(1, 13), rand.Next(1, 30),
+                newParcel.Requested = new DateTime(2021, rand.Next(1, 13), rand.Next(1, 30),
                     rand.Next(24), rand.Next(60), rand.Next(60));
                 int status = rand.Next(100);
                 int drone = -1;
                 if (status >= 10)
                 {
                     //Scheduling a time to deliver parcel
-                    Parcels[index].Scheduled = Parcels[index].Requested +
+                    newParcel.Scheduled = Parcels[index].Requested +
                         new TimeSpan(rand.Next(5), rand.Next(60), rand.Next(60));
                     if (status >= 15)
                     {
                         //Time drone came to deliver parcel
-                        Parcels[index].PickedUp = Parcels[index].Scheduled +
+                        newParcel.PickedUp = Parcels[index].Scheduled +
                             new TimeSpan(0, rand.Next(1, 60), rand.Next(60));
                         if (status >= 20)
                         {
                             //Time customer recieved parcel
-                            Parcels[index].Delivered = Parcels[index].PickedUp +
+                            newParcel.Delivered = Parcels[index].PickedUp +
                                 new TimeSpan(0, rand.Next(1, 60), rand.Next(60));
                             do
                             {
                                 drone = rand.Next(10);
-                                Parcels[index].DroneId = Drones[drone].Id;
+                                newParcel.DroneId = Drones[drone].Id;
                             }
                             while (Drones[drone].MaxWeight < Parcels[index].Weight);
                         }
@@ -158,13 +167,14 @@ namespace DalObject
                         do
                         {
                             drone = rand.Next(10);
-                            Parcels[index].DroneId = Drones[drone].Id;
+                            newParcel.DroneId = Drones[drone].Id;
                         }
-                        while (Drones[drone].Status == DroneStatuses.Available &&
+                        while (/*Drones[drone].Status == DroneStatuses.Available &&*/
                             Drones[drone].MaxWeight >= Parcels[index].Weight);
                     }
-                    Drones[drone].Status = DroneStatuses.Delivery;
+                    //Drones[drone].Status = DroneStatuses.Delivery;
                 }
+                Parcels.Add(newParcel);
             }
             DataSource.Config.IndexParcel = 10;//Promoting the index
         }
