61d481e6fa4f2f78c0532dddf69abbf7626aed30
yiratb <yibiton@g.jct.ac.il>
2021-11-08 20:21:40 +0200

    shhhhhhhhh!!!!!!!!!1

---
+++ BL/Enum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    namespace BO
+    {
+        public class Enum
+        {
+            public enum WeightCategories { Easy = 1, Medium, Heavy };
+            public enum Priorities { Normal = 1, Fast, Emergency };
+            public enum DroneStatuses { Available = 1, Maintenance, Delivery };
+        }
+    }
+}
--- DAL/DalObjectDrone.cs
+++ DAL/DalObjectDrone.cs
@@ -48,5 +48,30 @@ namespace DalObject
             }
             return tempDrones;
         }
+        /// <summary>
+        /// Assigning a parcel to a drone
+        /// </summary>
+        /// <param name="idParcel">Parcel to assign to drone</param>
+        /// <param name="idDrone">Drone which will be assigned a parcel</param>
+        public void UpdateAssignParcelToDrone(int idParcel, int idDrone)
+        {
+            if (!DataSource.Parcels.Exists(item => item.Id == idParcel))
+                throw new DataExceptions("The parcel does not exist.\n");
+            if (!DataSource.Drones.Exists(item => item.Id == idDrone))
+                throw new DataExceptions("The drone does not exist.\n");
+            int indexAssign = 0;
+            Parcel newParcel = new();
+            //Drone newDrone = new();
+            while (DataSource.Parcels[indexAssign].Id != idParcel)//finds the placement of the next parcel
+                indexAssign++;
+            newParcel = DataSource.Parcels[indexAssign];
+            newParcel.DroneId = idDrone;//giving parcel available drones' id
+            newParcel.Scheduled = DateTime.Now;//updating date and time
+            DataSource.Parcels[indexAssign] = newParcel;
+            indexAssign = 0;
+            //while (DataSource.Drones[indexAssign].Id != idDrone)
+            //    indexAssign++;
+            //DataSource.Drones[indexAssign].Status = DroneStatuses.Delivery;//updating that drone is busy
+        }
     }
 }
