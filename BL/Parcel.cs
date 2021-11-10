0397bf5df4e88d995ee03bca1bf84292cd70bf85
pinskishira <pinski@g.jct.c.il>
2021-11-10 23:38:25 +0200

    added switch

--- BL/Parcel.cs
+++ BL/Parcel.cs
@@ -9,19 +9,18 @@ namespace IBL
 {
     namespace BO
     {
-        class Parcel
+        public class Parcel
         {
             public int Id { get; set; }
             public CustomerInParcel SenderId { get; set; }
             public CustomerInParcel TargetId { get; set; }
             public WeightCategories Weight { get; set; }
             public Priorities Priority { get; set; }
-            public DateTime Requested { get; set; }
+            public DateTime Requested { get; set; }//זמן יצירת חבילה
             public int DroneId { get; set; }
-            public DateTime CreatedParcel { get; set; }
-            public DateTime Scheduled { get; set; }
-            public DateTime Delivered { get; set; }
-            public DateTime PickedUp { get; set; }
+            public DateTime Scheduled { get; set; }//זמן שיוך חבילה
+            public DateTime PickedUp { get; set; }//זמן איסוף
+            public DateTime Delivered { get; set; }//זמן אספקה
             public override string ToString()
             {
                 String result = "";
@@ -32,7 +31,6 @@ namespace IBL
                 result += $"Priority is {Priority} \n";
                 result += $"Requested is {Requested} \n";
                 result += $"DroneId is {DroneId} \n";
-                result += $"CreatedParcel is {CreatedParcel} \n";
                 result += $"Scheduled is {Scheduled} \n";
                 result += $"Delivered is {Delivered} \n";
                 result += $"PickedUp is {PickedUp} \n";
--- ConsoleUI_BL/Class1.cs
+++ ConsoleUI_BL/Class1.cs
@@ -1,8 +1,123 @@
 ﻿using System;
+using IDAL;
+using System.Collections.Generic;
+using IBL.BO;
+using static IBL.BO.Enum;
 
 namespace ConsoleUI_BL
 {
-    public class Class1
+    public enum BlMainSwitchFunctions { Add = 1, Update, Display, ListView, Exit };
+    public enum AddingFunction { AddStation = 1, AddDrone, AddCustomer, AddParcel };
+    public enum UpdateingFunction { AssignParcelToDrone = 1, ParcelCollectionByDrone, ParcelDeliveryToCustomer, SendDroneToChargingStation, DroneReleaseFromChargingStation };
+    public enum DisplayingFunction { Station = 1, Drone, Customer, Parcel };
+    public enum ListViewFunction { Stations = 1, Drones, Customers, Parcels, ParcelsWithNoDrone, StationWithAvailableChargingStation };
+
+    class Class1
     {
+        public static void Main(string[] args)
+        {
+            BL.BL ibl = new BL.BL();
+            int ansFromUserInt, input;
+            double ansFromUserDouble1;
+            double ansFromUserDouble2;
+            AddingFunction answerAdd;
+            BlMainSwitchFunctions answerMain;
+            DisplayingFunction answerDisplay;
+            UpdateingFunction answerUpdate;
+            ListViewFunction answerListView;
+
+            do
+            {
+                Console.WriteLine("What would you like to perform?\nEnter 1 to Add \nEnter 2 to Update \n" +
+                    "Enter 3 to Display \nEnter 4 to ListView \nEnter 5 to Exit \n");
+                int.TryParse(Console.ReadLine(), out input);
+                answerMain = (BlMainSwitchFunctions)input;
+                try
+                {
+                    switch (answerMain)
+                    {
+                        case (BlMainSwitchFunctions)AddingFunction.AddStation://case which adds a new station with data into the Stations array
+                            Station newStation = new();
+                            Console.Write("Enter new station Id: ");
+                            int.TryParse(Console.ReadLine(), out input);
+                            newStation.Id = input;
+                            Console.Write("Enter new station name: ");
+                            newStation.NameOfStation = Console.ReadLine();
+                            Console.Write("Please enter you location: ");
+                            double.TryParse(Console.ReadLine(), out ansFromUserDouble1);
+                            double.TryParse(Console.ReadLine(), out ansFromUserDouble2);
+                            newStation.StationLocation.Latitude = ansFromUserDouble1;
+                            newStation.StationLocation.Longitude = ansFromUserDouble1;
+                            Console.Write("Enter amount of charge slots in new station : ");
+                            int.TryParse(Console.ReadLine(), out input);
+                            newStation.AvailableChargeSlots = input;
+                            //newStation.DronesInCharging = 0;
+                            ibl.AddStation(newStation);
+                            break;
+                        case (BlMainSwitchFunctions)AddingFunction.AddDrone://case which adds a new drone with data into the Drones array
+                            Drone newDrone = new();
+                            Console.Write("Enter drone Id: ");
+                            int.TryParse(Console.ReadLine(), out input);
+                            newDrone.Id = input;
+                            Console.Write("Enter drone model: ");
+                            newDrone.Model = Console.ReadLine();
+                            Console.WriteLine("Enter drones' maximum weight:\n1 - Easy\n2 - Medium\n3 - Heavy");
+                            int.TryParse(Console.ReadLine(), out input);
+                            newDrone.MaxWeight = (WeightCategories)input;
+                            Console.Write("Enter the drones' battery status: ");
+                            int.TryParse(Console.ReadLine(), out input);
+                            //newDrone.Battery = input;
+                            ibl.AddDrone(newDrone);
+                            break;
+                        case (BlMainSwitchFunctions)AddingFunction.AddCustomer://case which adds a new customer with data into the Customers array
+                            Customer NewCustomer = new();
+                            Console.Write("Please enter ID: ");
+                            int.TryParse(Console.ReadLine(), out ansFromUserInt);
+                            NewCustomer.Id = ansFromUserInt;
+                            Console.Write("Please enter name: ");
+                            NewCustomer.Name = Console.ReadLine();
+                            Console.Write("Please enter phone: ");
+                            NewCustomer.Phone = Console.ReadLine();
+                            Console.Write("Please enter you location: ");
+                            double.TryParse(Console.ReadLine(), out ansFromUserDouble1);
+                            double.TryParse(Console.ReadLine(), out ansFromUserDouble2);
+                            NewCustomer.CustomerLocation.Latitude = ansFromUserDouble1;
+                            NewCustomer.CustomerLocation.Longitude = ansFromUserDouble1;
+                            ibl.AddCustomer(NewCustomer);
+                            break;
+                        case (BlMainSwitchFunctions)AddingFunction.AddParcel://case which adds a new parcel with data into the Parcels array
+                            Parcel NewParcel = new();
+                            NewParcel.Id = 0;
+                            Console.Write("Enter sender ID of 9 digits: ");
+                            int.TryParse(Console.ReadLine(), out ansFromUserInt);
+                            NewParcel.SenderId.Id = ansFromUserInt;
+                            Console.Write("Enter sender's name: ");
+                            int.TryParse(Console.ReadLine(), out input);
+                            NewParcel.SenderId.Name = input;
+                            Console.Write("Enter target ID of 9 digits: ");
+                            int.TryParse(Console.ReadLine(), out ansFromUserInt);
+                            NewParcel.TargetId.Id = ansFromUserInt;
+                            Console.Write("Enter target's name: ");
+                            int.TryParse(Console.ReadLine(), out input);
+                            NewParcel.TargetId.Name = input;
+                            Console.WriteLine("Enter the weight of your parcel:\n1 - Easy\n2 - Medium\n3 - Heavy");
+                            int.TryParse(Console.ReadLine(), out input);
+                            NewParcel.Weight = (WeightCategories)input;
+                            Console.WriteLine("Enter the urgency of your parcel:\n1 - normal\n2 - Fast\n3 - Emergency");
+                            int.TryParse(Console.ReadLine(), out input);
+                            NewParcel.Priority = (Priorities)input;
+                            NewParcel.DroneId = 0;
+                            dalObj.AddParcel(NewParcel);
+                            break;
+                    }
+                    break;
+                }
+                catch ()
+                {
+
+                }
+            }
+            while (answerMain != BlMainSwitchFunctions.Exit) ;
+        }
     }
 }
