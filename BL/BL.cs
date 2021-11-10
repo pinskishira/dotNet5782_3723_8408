84828d1fbff14e60d95de323c8ea5af51da84a84
yiratb <yibiton@g.jct.ac.il>
2021-11-10 17:21:27 +0200

    u

--- BL/BL.cs
+++ BL/BL.cs
@@ -1,8 +1,25 @@
 ﻿using System;
-
+using DAL;
+using DalObject;
+using IDAL;
+using IBL;
+using System.Collections.Generic;
+using BL;
+using DAL.IDAL.DO;
+    
 namespace BL
 {
     public class BL
     {
+        public BL()
+        {
+            IDal dalObject = new DalObject.DalObject();
+            double[] elecUse = dalObject.electricityUse();
+            double Available = elecUse[0];
+            double LightWeight = elecUse[01];
+            double MediumWeight = elecUse[2];
+            double HeavyWeight = elecUse[3];
+            double DroneLoadingRate = elecUse[4];
+        }
     }
 }
