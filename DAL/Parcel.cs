bff277869edd2555820d949e1d8df0419907943e
yiratb <yibiton@g.jct.ac.il>
2021-11-18 14:28:01 +0200

    i add somthing

--- DAL/Parcel.cs
+++ DAL/Parcel.cs
@@ -15,8 +15,8 @@ namespace IDAL
             public int TargetId { get; set; }
             public WeightCategories Weight { get; set; }
             public Priorities Priority { get; set; }
-            public DateTime Requested { get; set; }
             public int DroneId { get; set; }
+            public DateTime Requested { get; set; }
             public DateTime Scheduled { get; set; }
             public DateTime Delivered { get; set; }
             public DateTime PickedUp { get; set; }
